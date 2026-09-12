45b4b138e0cd54e8c91ec2132e583aefd382fd85
yash43211234 <yashwant4769@gmail.com>
2026-09-13 01:28:51 +0530

    version 1.1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import CodeCaster from './Projects/CodeCaster'
-import LiveCssTyper from './Projects/LiveCssTyper'
+// import CodeCaster from './Projects/CodeCaster'
+// import LiveCssTyper from './Projects/LiveCssTyper'
 import Playground from './Projects/Playground'
 
 export default function App() {
   return (
     <div>
-      
       <Playground/>
     </div>
   )
